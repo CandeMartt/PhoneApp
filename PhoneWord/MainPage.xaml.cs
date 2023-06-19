9222a7c20107b7d7c267a6bca049ac85d03a8e9e
CandeMartt <candemartt@gmail.com>
2023-06-19 17:06:51 -0300

    Esta aplicacion se relaciona con la aplicacion de llamadas del celular. Fue realizada con .Net Maui y C# en Visual Studio 2022

---
+++ PhoneWord/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿namespace PhoneWord;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    string translatedNumber;
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        // Invoco la clase que traduce
+        translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+        // Si no es nulo que hago lo siguiente
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Llamar a " + translatedNumber;
+        }
+        // Si es nulo que haga lo siguiente
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Llamar";
+        }
+    }
+    // Evento que realiza la llamada
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        // preguntara si quiere realizar la llamada
+        // Display Alert permite desplegar una ventana emergente
+        // Si la tuviera esta ventana emergente solo una opcion, como por ejemplo aceptar, no devolveria ningun valor
+        // Pero como la ventana emergente tiene dos opciones nos devuelve un booleano
+        if (await this.DisplayAlert(
+            "Marcado",
+            "¿Quieres llamar al " + translatedNumber + "?",
+            "Si",
+            "No"))
+        {
+            
+            // Utilizamos un try/catch por si algo falla
+            try
+            {
+                // Antes de marcar evalua si el telefono tiene la capacidad de marcar.
+                // Si puede marcar abre la opcion de marcado
+                if (PhoneDialer.Default.IsSupported)
+                    PhoneDialer.Default.Open(translatedNumber);
+                else
+                    await DisplayAlert("Error al llamar", "El dispositivo no permite llamadas", "Aceptar");
+                // Como solo tiene la opcion OK no devuelve ningun valor
+            }
+
+            // Si falla avisa que no se puede marcar porque el numero no es valido
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Error al llamar", "El número no es valido", "Aceptar");
+            }
+            // Si falla avisa que no se puede marcar. Este es un error mas general
+            catch (Exception)
+            {
+                // Other error has occurred.
+                await DisplayAlert("Error al llamar", "Error desconocido", "Aceptar");
+            }
+        }
+    }
+}
+
---
+++ PhoneWord/PhonewordTranslator.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace Core;
+
+public static class PhonewordTranslator
+{
+    // clase estatica que recibe una variable string
+    public static string ToNumber(string raw)
+    {
+        // Se fija que el campo no este en blanco
+        if (string.IsNullOrWhiteSpace(raw))
+            return null;
+        // El string que recibe lo convierte en mayusculas
+        raw = raw.ToUpperInvariant();
+
+        // A partir de aqui hace la conversión
+        var newNumber = new StringBuilder();
+        foreach (var c in raw)
+        {
+            // Si es un numero lo mantiene
+            if (" -0123456789".Contains(c))
+                newNumber.Append(c);
+
+            // Si se puso un caracter especial como un * o una coma o un punto devuelve un nulo
+            else
+            {
+                var result = TranslateToNumber(c);
+                if (result != null)
+                    newNumber.Append(result);
+                // Bad character?
+                else
+                    return null;
+            }
+        }
+        return newNumber.ToString();
+    }
+
+    static bool Contains(this string keyString, char c)
+    {
+        return keyString.IndexOf(c) >= 0;
+    }
+
+    // Traduccion de las letras como en los telefonos antiguos. 
+    static readonly string[] digits = {
+        "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+    };
+
+    // Si recibe una letra lo convertira a numero.
+    static int? TranslateToNumber(char c)
+    {
+        for (int i = 0; i < digits.Length; i++)
+        {
+            if (digits[i].Contains(c))
+                return 2 + i;
+        }
+        return null;
+    }
+}
